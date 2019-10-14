4b202fefa876782759411c152ccd2db302145fd4
Patrik Kullman <patrik.kullman@neovici.se>
2019-10-14 13:52:38 +0200

    fix(ci): report file as sourcepath, not in msg

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const { logIssue, IssueType } = require("azure-pipelines-task-lib");
 module.exports = function (wct, pluginOptions) {
   wct.on('test-end', function (browser, test) {
     if (test.state !== 'failing') return;
-
-    logIssue(IssueType.Error, `Test ${test.test.join(' » ')} failed.`)
+    
+    const file = test.test.splice(0, 1);
+    logIssue(IssueType.Error, `Test ${test.test.join(' » ')} failed.`, file);
   }.bind(this));
 };
